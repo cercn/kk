3711376374d3b4f663f2db29bb722238547e2d5d
GangloUlrich <gangloulrichluc@gmail.com>
2022-02-06 23:05:45 +0100

    Update showproduct.blade.php

--- resources/views/admin/products/showproduct.blade.php
+++ resources/views/admin/products/showproduct.blade.php
@@ -58,3 +58,36 @@
     </div>
   </div>
 @endsection
+
+
+@push('scripts')
+    <script type="text/javascript" src="https://static.sketchfab.com/api/sketchfab-viewer-1.12.0.js"></script>
+
+    <script type="text/javascript">
+        var iframe = document.getElementById('api-frame');
+        var uid = '7w7pAfrCfjovwykkEeRFLGw5SXS';
+
+        // By default, the latest version of the viewer API will be used.
+        var client = new Sketchfab(iframe);
+
+        // Alternatively, you can request a specific version.
+        // var client = new Sketchfab( '1.12.0', iframe );
+
+        client.init(uid, {
+            success: function onSuccess(api) {
+                api.start();
+                api.addEventListener('viewerready', function() {
+
+                    // API is ready to use
+                    // Insert your code here
+                    console.log('Viewer is ready');
+
+                });
+            },
+            error: function onError() {
+                console.log('Viewer error');
+            }
+        });
+    </script>
+
+@endpush
