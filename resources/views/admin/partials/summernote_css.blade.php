f4890cb3dbdf5b3d70e8074c3fad363a44d57899
GangloUlrich <gangloulrichluc@gmail.com>
2022-03-04 17:34:52 +0100

    retrait de summernote et ajout de tinyMCE

--- resources/views/admin/partials/summernote_css.blade.php
+++
@@ -1,6 +0,0 @@
-@push('stylesheets')
-<link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
-{{-- summernote --}}
-<link rel="stylesheet" href="{{asset('summernote/summernote-bs4.css')}}">
-
-@endpush
